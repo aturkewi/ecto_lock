e08f205bf0a52e6217f314a349f4213886a4a273
Avidor Turkewitz <aviturkewitz@gmail.com>
2019-01-22 13:45:02 -0500

    feeling the pain

--- lib/ecto_lock/helper.ex
+++ lib/ecto_lock/helper.ex
@@ -7,4 +7,9 @@ defmodule EctoLock.Helper do
     BillPendingInvoices.create_pending_invoice()
     BillPendingInvoices.create_pending_invoice()
   end
+
+  def bill_from_two_servers() do
+    spawn(fn -> BillPendingInvoices.bill_pending_invoices() end)
+    spawn(fn -> BillPendingInvoices.bill_pending_invoices() end)
+  end
 end
